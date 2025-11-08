61d69df85947ea4f710e4d51506152eba115a119
karlos <karlosmarques@gmail.com>
2025-11-08 17:06:01 -0300

    cors enableCors adicionado

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+    app.enableCors({
+    origin: 'http://localhost:3000', //Dafni, se a porta que vcs estiverem usando for a 3000, pode deixar assim, mas se for outra, alterem, pfv! 😉
+    credentials: true
+  });
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
