2dcc725bee48017aefae635a8f83d8020d5221c6
Isaacpessoadias <ipdg@discente.ifpe.edu.br>
2025-11-10 17:18:33 -0300

    perfil no module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { RestauranteModule } from './restaurante/restaurante.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { AuthModule } from './auth/auth.module';
+import { PerfilModule } from './perfil/perfil.module';
 
 @Module({
-  imports: [RestauranteModule,PrismaModule,RestauranteModule, CategoriaModule,AuthModule],
+  imports: [RestauranteModule,PrismaModule,RestauranteModule, CategoriaModule,AuthModule, PerfilModule],
   controllers: [AppController],
   providers: [AppService],
 })
