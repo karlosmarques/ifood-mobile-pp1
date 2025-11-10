85eb1dfef9c66fc9685d250530d65f56aab966e3
Isaacpessoadias <ipdg@discente.ifpe.edu.br>
2025-11-10 15:54:02 -0300

    migração resetada

---
+++ prisma/migrations/20251110184703_dev/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `categoria` ADD COLUMN `imagemUrl` VARCHAR(255) NULL;
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-    origin: 'http://localhost:3000', //Dafni, se a porta for essa pode deixar 3000, mas se for outra troca 😉
+    origin: 'http://localhost:8081', //Dafni, se a porta for essa pode deixar 3000, mas se for outra troca 😉
     credentials: true
   });
   await app.listen(process.env.PORT ?? 8000);
